154f256dfd43c24b1c8d966ff7f776ab011e6528
aliaulia <alikibow@gmail.com>
2020-06-03 23:06:08 +0700

    API register done

--- server.js
+++ server.js
@@ -102,13 +102,11 @@ function checkEmail(email){
         sqlGetUserEmail = "SELECT * FROM users WHERE email = ?";
         db.query(sqlGetUserEmail, [email] , function (err ,result, fields) {
             if (err) throw err;
-            // console.log('================ result ================')
-            console.log(result)
             if(Array.isArray(result) && result.length > 0){
                 reject('emailExist');
             }
             else{
-                resolve('emailNotExist');
+                resolve('accept');
             }
         });
     });
@@ -119,15 +117,29 @@ function checkUsername(username){
         sqlGetUserEmail = "SELECT * FROM users WHERE username = ?";
         db.query(sqlGetUserEmail, [username] , function (err ,result, fields) {
             if (err) throw err;
-            // console.log('================ result ================')
-            console.log(result)
             if(Array.isArray(result) && result.length > 0){
                 reject('usernameExist');
             }
             else{
-                resolve('usernameNotExist');
+                resolve('accept');
             }
         });
+    });
+}
+
+function checkPassword(password){
+    return new Promise((resolve, reject) => {
+        if(password.length >= 6 && password.length <= 32){
+            const passwordFilter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[ !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}])[A-Za-z\d !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}]{6,32}$/g;
+            const isPasswordFiltered = passwordFilter.exec(password);
+            if(!isPasswordFiltered){
+                reject("passwordFailure")
+            }
+        }
+        else{
+            reject("passwordLength")
+        }
+        resolve("accept")
     });
 }
 
@@ -174,77 +186,64 @@ app.post('/diary/register', async (req, res) => {
         user_username = req.body.username; 
         user_password = req.body.password; 
 
-        // Query
-        sqlGetUserEmail = "SELECT * FROM users WHERE email = \""+ user_email+"\"";
-        sqlGetUserUsername = "SELECT * FROM users WHERE username = \""+user_username+"\"";
-        sqlInsertUser = "INSERT INTO users (fullname, birthday, email, username, password) VALUES ?";
-
-        isFailed = false;
-        message = "";
-        
         const emailResult = await checkEmail(user_email);
-        // const emailResult = await db.query(sqlGetUserEmail);
-        // console.log('==================== emailResult ========================');
-        // console.log(emailResult);
-        // if(emailResult){
-        //     isFailed = true;
-        //     message += "Registered email already exist. Please use other email."
-        // }
-
         const usernameResult = await checkUsername(user_username);
+        const passwordResult = await checkPassword(user_password);
+        
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(user_password, salt)
 
-        // const usernameResult = await db.query(sqlGetUserUsername);
-        // console.log('==================== usernameResult ========================');
-        // console.log(usernameResult);
-        // if(usernameResult){
-        //     isFailed = true;
-        //     message += "\nUsername already taken. Please try another username."
-        // }
-
-        if(user_password.length > 6 && user_password.length < 32){
-            const passwordFilter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[ !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}])[A-Za-z\d !"#$%&'()*+,-.\/:;<=>?@[\]^_`{|}]{6,32}$/g;
-            const isPasswordFiltered = passwordFilter.exec(user_password);
-            if(!isPasswordFiltered){
-                isFailed = true;
-                message += "\nPassword must have contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and one special character."
-            }
-        }
-        else{
-            isFailed = true
-            message += "\nPassword must have length between 6-32 characters." 
-        }
-
-        if(isFailed){
+        // INSERT NEW USER
+        sqlInsertUser = "INSERT INTO users (fullname, birthday, email, username, password) VALUES ?";
+        var value = [
+            [user_fullname, user_birthday, user_email, user_username, hashedPassword]
+        ];
+        db.query(sqlInsertUser, [value] ,function (err, result, fields) {
+            if (err) throw err;
+            res.status(201);
+            res.json({
+                success: true,
+                message: "Success register new user."
+            });
+        });
+    }
+    catch (err){
+        message = "";
+        if(err === 'emailExist'){
             res.status(201);
             res.json({
                 success: false,
-                message: message,
+                message: "Registered email already exist. Please use other email."
+            });
+        }
+        else if(err === 'usernameExist'){
+            res.status(201);
+            res.json({
+                success: false,
+                message: "Username already taken. Please try another username."
+            });
+        }
+        else if(err === 'passwordLength'){
+            res.status(201);
+            res.json({
+                success: false,
+                message: "Password must have length between 6-32 characters."
+            });
+        }
+        else if(err === 'passwordFailure'){
+            res.status(201);
+            res.json({
+                success: false,
+                message: "Password must have contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and one special character."
             });
         }
         else{
-            const salt = await bcrypt.genSalt()
-            const hashedPassword = await bcrypt.hash(user_password, salt)
-
-            // INSERT SQL
-            var value = [
-                [user_fullname, user_birthday, user_email, user_username, hashedPassword]
-            ];
-            db.query(sqlInsertUser, [value] ,function (err, result, fields) {
-                if (err) throw err;
-                res.status(201);
-                res.json({
-                    success: true,
-                    message: "Success register new user."
-                });
+            res.status(500);
+            res.json({
+                success: false,
+                message: "Internal Error 500"
             });
         }
-    }
-    catch (err){
-        res.status(500);
-        res.json({
-            success: false,
-            message: "Internal Error 500"
-        });
     }
 });
 
